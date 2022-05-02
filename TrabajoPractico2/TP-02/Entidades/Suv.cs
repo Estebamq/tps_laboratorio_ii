8094d923b1b0d5bd3f6173bf7e04cbb4460d05bf
marcelo quiroz <quiroz_estebanmarcelo@live.com>
2022-05-02 00:53:51 -0300

    agrego tp nº2

---
+++ TrabajoPractico2/TP-02/Entidades/Suv.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+
+namespace Entidades
+{
+    public class Suv:Vehiculo
+    {
+       
+        public Suv (EMarca marca, string chasis, ConsoleColor color)
+            : base(marca, chasis, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+        
+
+        
+        /// <summary>
+        /// Muestra los datos del SUV
+        /// </summary>
+        /// <returns>retorna una cadena de datos del SUV</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        
+    }
+}
